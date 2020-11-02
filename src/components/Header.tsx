91afcd17a8bb67780abbce0ff3e5e7530acafb00
buba1301 <bmuza57@gmail.com>
2020-11-03 00:24:03 +0300

    Add body styles and fix Header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,18 +8,26 @@ const Header = () => {
       <div className={cn(s.groupe95)}>
         <img src="/public/logo.png" className={cn(s.logo)} />
         <nav className={cn(s.navbar)}>
-          <a href="/" className={cn(s.menuItemActive)}>
-            <span className={cn(s.home)}>Home</span>
-          </a>
-          <a href="/poke" className={s.menuItem1}>
-            <span className={cn(s.pokedex)}>Pokedex</span>
-          </a>
-          <a href="/legend" className={s.menuItem2}>
-            <span className={cn(s.legendaries)}>Legendaries</span>
-          </a>
-          <a href="/doc" className={s.menuItem3}>
-            <span className={cn(s.documentation)}>Documentation</span>
-          </a>
+          <div className={cn(s.menuItemActive)}>
+            <a href="/" className={cn(s.home)}>
+              Home
+            </a>
+          </div>
+          <div className={s.menuItem1}>
+            <a href="/poke" className={cn(s.pokedex)}>
+              Pokedex
+            </a>
+          </div>
+          <div className={s.menuItem2}>
+            <a href="/legend" className={cn(s.legendaries)}>
+              Legendaries
+            </a>
+          </div>
+          <div className={s.menuItem3}>
+            <a href="/doc" className={cn(s.documentation)}>
+              Documentation
+            </a>
+          </div>
         </nav>
       </div>
       <div className={cn(s.headerContainer)}>
