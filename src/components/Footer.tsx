5d9630c8673552577f10563360846786aa1a561a
buba1301 <bmuza57@gmail.com>
2020-11-03 00:23:07 +0300

    Create Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import cn from 'classnames';
-// import s from './Footer.modul.scss';
+import s from './Footer.module.scss';
 
 const Footer = () => {
   return (
     <footer>
-      <div className={cn()}>
-        <span>Make with Love</span>
-        <span>Our team</span>
+      <div className={cn(s.footerText)}>
+        <div className={cn(s.makeWithLove)}>
+          <p className={cn(s.h3, s.pharagraph)}>Make with Love</p>
+        </div>
+        <div className={s.ourTeam}>
+          <p className={cn(s.h3, s.pharagraph)}>Our team</p>
+        </div>
       </div>
     </footer>
   );
