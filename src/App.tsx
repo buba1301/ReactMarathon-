7c1883e82fcf364d685bbab72c58c47071a62003
buba1301 <bmuza57@gmail.com>
2020-11-02 20:18:57 +0300

    Add ESLint abd Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,7 @@ import cn from 'classnames';
 import s from './App.module.scss';
 
 const App = () => {
-
-	console.log('djfhjdhf');
-
-	return (
-		<div className={cn(s.header, ' color')}>
-			This is React App
-		</div>
-	)
+  return <div className={cn(s.header, ' color')}>This is React App</div>;
 };
 
 export default App;
