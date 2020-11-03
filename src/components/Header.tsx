b11775013d8d231f261612a50782439f9ec8bf64
buba1301 <bmuza57@gmail.com>
2020-11-04 00:09:32 +0300

    change header to flex container

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,6 +5,43 @@ import s from './Header.module.scss';
 const Header = () => {
   return (
     <header className={cn(s.header)}>
+      <div className={cn(s.logo, s.w30)}>
+        <img src="/public/logo.png" alt="Pockemon" />
+      </div>
+
+      <nav className={cn(s.w70)}>
+        <ul className={cn(s.nav)}>
+          <li className={cn(s.li)}>
+            <a href="/" className={cn(s.font)}>
+              Home
+            </a>
+          </li>
+          <li className={s.li}>
+            <a href="/poke" className={cn(s.font)}>
+              Pokedex
+            </a>
+          </li>
+          <li className={s.li}>
+            <a href="/legend" className={cn(s.font)}>
+              Legendaries
+            </a>
+          </li>
+          <li className={s.li}>
+            <a href="/404" className={cn(s.font)}>
+              Documentation
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
+
+/*
+  return (
+    <header className={cn(s.header)}>
       <div className={cn(s.groupe95)}>
         <div className={cn(s.logo)}>
           <img src="/public/logo.png" alt="Pockemon" className={cn(s.groupe)} />
@@ -40,6 +77,4 @@ const Header = () => {
       </div>
     </header>
   );
-};
-
-export default Header;
+*/
