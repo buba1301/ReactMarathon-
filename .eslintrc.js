93f4ccba176a43148ff22b58f30adb7441c9dd53
buba1301 <bmuza57@gmail.com>
2020-11-08 13:44:45 +0300

    Add console.log rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
   },
   plugins: ['prettier', 'prettier', 'react', '@typescript-eslint'],
   rules: {
+    'no-console': 'on',
     'react/prop-types': 'off',
     '@typescript-eslint/no-unused-vars': 'error',
     'no-console': 'error',
