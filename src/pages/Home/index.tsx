499f17b63aa53705c3b99396af4af633812e8163
buba1301 <bmuza57@gmail.com>
2020-11-08 13:56:59 +0300

    Add Layout component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,11 @@
+/* eslint-disable no-console */
 import React from 'react';
 import Header from '../../components/Header/Header';
 import Layout from '../../components/Layout/Layout';
 import Footer from '../../components/Footer/Footer';
+import Button from '../../components/Button/Index';
+import Parallax from '../../components/Parallax/Parallax';
+import Heading from '../../components/Heading/Heading';
 
 import s from './Home.module.scss';
 
@@ -9,7 +13,22 @@ const HomePage = () => {
   return (
     <div className={s.root}>
       <Header />
-      <Layout />
+      <Layout className={s.contentWrap}>
+        <div className={s.contentText}>
+          <Heading className={s.heading1}>
+            <b>Find</b> all your favorite <b>Pokemon</b>
+          </Heading>
+          <Heading className={s.heading2}>
+            You can know the type of Pokemon, its strengths, disadvantages and abilities
+          </Heading>
+          <Button onClick={() => console.log('Button click!')} className={s.pockeButton}>
+            See pokemons
+          </Button>
+        </div>
+        <div className={s.contentParallax}>
+          <Parallax />
+        </div>
+      </Layout>
       <Footer />
     </div>
   );
