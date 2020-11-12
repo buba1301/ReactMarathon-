d019556a24cc6635b4428b731d6158d7f3095dd5
buba1301 <bmuza57@gmail.com>
2020-11-12 17:48:00 +0300

    Add keyof for color and size type

--- src/components/Button/Index.tsx
+++ src/components/Button/Index.tsx
@@ -3,19 +3,15 @@ import cn from 'classnames';
 
 import s from './Button.module.scss';
 
-type Size = 'small' | 'normal';
-
-type Color = 'yellow' | 'green';
-
 interface ButtonProps {
   onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
   block?: boolean;
-  color?: Color;
-  size: Size;
+  color?: string;
+  size: string;
 }
 
 const Button: React.FC<ButtonProps> = ({ children, onClick, block = false, color = 'green', size }) => {
-  const classNames = cn(block, color, s[size], s[color], s.root);
+  const classNames = cn(block, color, s[size as keyof typeof s], s[color as keyof typeof s], s.root);
 
   return (
     <button type="button" className={classNames} onClick={onClick}>
