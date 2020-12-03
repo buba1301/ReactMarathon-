ad8f41faa780388e3c0a5aee0460f5c8fb99d2be
Boris Muzalevskii <bmuza57@gmail.com>
2020-12-03 16:15:13 +0300

    Add finterNames utils

---
+++ src/utils/filterNames.ts
@@ -0,0 +1,22 @@
+const filterByTypes = [
+  "bug",
+  "dark",
+  "dragon",
+  "electric",
+  "normal",
+  "rock",
+  "fairy",
+  "fighting",
+  "fire",
+  "flying",
+  "poison",
+  "steel",
+  "ghost",
+  "glass",
+  "ground",
+  "ice",
+  "psychic",
+  "water",
+];
+
+export default filterByTypes;
