a8d1f4387392569b44df29a6183c6527390c65c9
Boris Muzalevskii <bmuza57@gmail.com>
2021-04-08 17:39:20 +0300

    Fix upercase to lowercase files name in paralax assets

--- src/components/Parallax/Parallax.tsx
+++ src/components/Parallax/Parallax.tsx
@@ -1,13 +1,13 @@
 /* eslint-disable import/no-unresolved */
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
-import s from "./Parallax.module.scss";
+import s from './Parallax.module.scss';
 
-import SmallPokeBallPng from "./assets/Pokeballsmall.png";
-import CloudPng from "./assets/Cloudsmall.png";
-import PokeBallPng from "./assets/Pokeballbig.png";
-import CloudBigPng from "./assets/Cloudbig.png";
-import PikachuPng from "./assets/Pikachu.png";
+import SmallPokeBallPng from './assets/pokeballsmall.png';
+import CloudPng from './assets/cloudsmall.png';
+import PokeBallPng from './assets/pokeballbig.png';
+import CloudBigPng from './assets/cloudbig.png';
+import PikachuPng from './assets/pikachu.png';
 
 const Parallax = () => {
   const [screenX, setScreenX] = useState(0);
@@ -19,9 +19,9 @@ const Parallax = () => {
       setScreenY(event.screenY);
     };
 
-    window.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener('mousemove', handleMouseMove);
 
-    return () => window.removeEventListener("mousemove", handleMouseMove);
+    return () => window.removeEventListener('mousemove', handleMouseMove);
   }, [screenX, screenY]);
 
   return (
@@ -30,40 +30,35 @@ const Parallax = () => {
         className={s.smallPokeBall}
         style={{
           transform: `translate(${screenX * 0.02}px, ${screenY * 0.02}px)`,
-        }}
-      >
+        }}>
         <img src={SmallPokeBallPng} alt="Small PokeBall" />
       </div>
       <div
         className={s.cloud}
         style={{
           transform: `translate(${screenX * 0.01}px, ${screenY * 0.01}px)`,
-        }}
-      >
+        }}>
         <img src={CloudPng} alt="Cloud PokeBall" />
       </div>
       <div
         className={s.cloudBig}
         style={{
           transform: `translate(${screenX * 0.02}px, ${screenY * 0.02}px)`,
-        }}
-      >
+        }}>
         <img src={CloudBigPng} alt="Cloud Big PokeBall" />
       </div>
       <div
         className={s.pokeBall}
         style={{
           transform: `translate(${screenX * 0.03}px, ${screenY * 0.03}px)`,
-        }}
-      >
+        }}>
         <img src={PokeBallPng} alt="Big PokeBall" />
       </div>
       <div
         className={s.pikachu}
         style={{
           transform: `translate(${screenX * 0.04}px, ${screenY * -0.02}px)`,
-        }}
-      >
+        }}>
         <img src={PikachuPng} alt="Cloud PokeBall" />
       </div>
     </div>
